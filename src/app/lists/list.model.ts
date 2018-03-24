2498a286f2c48df73a0162378f9b873d66ddc6da
Alexey Popov <alexey.popov@umbrella-web.com>
2018-03-24 19:16:35 +0300

    #1 updated lodash and list model

--- src/app/lists/list.model.ts
+++ src/app/lists/list.model.ts
@@ -1,4 +1,5 @@
-import { get } from 'lodash';
+import * as uuidv4 from 'uuid/v4';
+import get from 'lodash-es/get';
 import { AbstractModel } from '../shared/model/abstract.model';
 
 export class ListModel extends AbstractModel {
@@ -8,7 +9,7 @@ export class ListModel extends AbstractModel {
 
   constructor(data) {
     super();
-    if (get(data, 'id')) this.id = data.id;
+    this.id = get(data, 'id') ? data.id : uuidv4();
     this.name = get(data, 'name');
     this.todos = [];
   }
