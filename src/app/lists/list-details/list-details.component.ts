28dc6f35d52fd3be8a23d633b18b604999b84399
Alexey Popov <alexey.popov@umbrella-web.com>
2018-03-24 19:27:46 +0300

    #1 made add todo using Enter key

--- src/app/lists/list-details/list-details.component.ts
+++ src/app/lists/list-details/list-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ListModel } from '../list.model';
 
 @Component({
   selector: 'app-product-add-edit',
@@ -6,6 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-details.component.scss'],
 })
 export class ListDetailsComponent implements OnInit {
+  todoForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  setForm(product: ListModel = ListModel.create()) {
+    this.todoForm = this.formBuilder.group({
+      name: [product.name || '', Validators.compose([
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(20),
+      ])],
+    });
+  }
+
   ngOnInit(): void {
+    this.setForm();
+  }
+
+  addTodo() {
+    if (this.todoForm.valid) {
+      console.log(this.todoForm.value);
+    }
   }
 }
