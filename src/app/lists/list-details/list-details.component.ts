8b9a7daa8386e72e582bb0e780fc233338b6a635
Alexey Popov <alexey.popov@umbrella-web.com>
2018-03-24 23:35:34 +0300

    #1 made detect filter changes and update url query string and exec redirect in case empty url query string filter

--- src/app/lists/list-details/list-details.component.ts
+++ src/app/lists/list-details/list-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Filters } from '../filtes.enum';
 import { TodoModel } from '../todo.model';
@@ -9,24 +10,31 @@ import { TodoModel } from '../todo.model';
   styleUrls: ['./list-details.component.scss'],
 })
 export class ListDetailsComponent implements OnInit {
+  routeId: number;
   filterForm: FormGroup;
   todoForm: FormGroup;
   filters = Filters;
   todos: TodoModel[] = [TodoModel.create({ name: 'Apple', listId: 9 })];
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) {
+    this.routeId = +activatedRoute.snapshot.params.id;
   }
 
-  setForms(): void {
+  initFilterForm() {
+    const { search = '', filter = '' } = this.activatedRoute.snapshot.queryParams;
     this.filterForm = this.formBuilder.group({
-      search: ['', Validators.compose([
+      search: [search, Validators.compose([
         Validators.required,
         Validators.minLength(2),
         Validators.maxLength(20),
       ])],
-      filter: ['all', Validators.required],
+      filter: [filter, Validators.required],
     });
+  }
 
+  setTodoForm(): void {
     this.todoForm = this.formBuilder.group({
       name: ['', Validators.compose([
         Validators.required,
@@ -36,8 +44,28 @@ export class ListDetailsComponent implements OnInit {
     });
   }
 
+  redirect(data) {
+    const { filter, search } = data;
+    const queryParams = { filter: filter || 'all', ...(search && { search }) };
+    this.router.navigate([`/lists/${this.routeId}`], { queryParams });
+  }
+
+  detectFilterChanges() {
+    // detect filter changes and update url query string
+    const filtersObserver = {
+      next: data => this.redirect(data),
+      error: error => console.error(error),
+    };
+    this.filterForm.valueChanges.subscribe(filtersObserver);
+  }
+
   ngOnInit(): void {
-    this.setForms();
+    // exec redirect in case empty url query string filter
+    this.redirect(this.activatedRoute.snapshot.queryParams);
+
+    this.initFilterForm();
+    this.setTodoForm();
+    this.detectFilterChanges();
   }
 
   addTodo() {
